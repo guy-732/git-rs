de5f7409d3fced1e6291ece81ab07bb02b1acce1
guy-732 <81652178+guy-732@users.noreply.github.com>
2024-04-01 01:48:47 +0200

    refactored to use clap

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,39 @@
-use std::env;
 use std::fs;
 
+use clap::{Parser, Subcommand};
+
+#[derive(Debug)]
+#[derive(Parser)]
+#[command(version, about = "git cmd tool in rust", long_about = None)]
+struct Cli {
+    #[command(subcommand)]
+    command: Command
+}
+
+#[derive(Debug)]
+#[derive(Subcommand)]
+enum Command {
+    /// Initialize a git repository in the current directory
+    Init,
+}
+
 fn main() {
-    let args: Vec<String> = env::args().collect();
-    if args[1] == "init" {
-        fs::create_dir(".git").unwrap();
-        fs::create_dir(".git/objects").unwrap();
-        fs::create_dir(".git/refs").unwrap();
-        fs::write(".git/HEAD", "ref: refs/heads/main\n").unwrap();
-        println!("Initialized git directory")
-    } else {
-        println!("unknown command: {}", args[1])
+    let cli = Cli::parse();
+    // eprintln!("{:#?}", cli);
+    match cli.command {
+        Command::Init => match init() {
+            Ok(_) => (),
+            Err(e) => eprintln!("Failed to initialize git repository: {:?}", e),
+        },
     }
+}
+
+fn init() -> anyhow::Result<()> {
+    fs::create_dir(".git")?;
+    fs::create_dir(".git/objects")?;
+    fs::create_dir(".git/refs")?;
+    fs::write(".git/HEAD", "ref: refs/heads/main\n")?;
+    println!("Initialized git directory");
+
+    Ok(())
 }
