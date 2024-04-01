3a1ea9ff71782a30dcc7052dd35e0b01947e77ab
guy-732 <81652178+guy-732@users.noreply.github.com>
2024-04-01 02:31:37 +0200

    solve stage 2

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,7 @@
-use std::fs;
+use std::{fs::{self, File}, io::{BufRead, BufReader}, path::PathBuf};
 
 use clap::{Parser, Subcommand};
+use flate2::read::ZlibDecoder;
 
 #[derive(Debug)]
 #[derive(Parser)]
@@ -15,6 +16,14 @@ struct Cli {
 enum Command {
     /// Initialize a git repository in the current directory
     Init,
+    /// Provide content of repository object
+    CatFile {
+        /// The hash of the object to inspect
+        object: String,
+        /// Pretty-print the contents of <object> based on its type.
+        #[arg(short)]
+        pretty_print: bool,
+    },
 }
 
 fn main() {
@@ -23,8 +32,12 @@ fn main() {
     match cli.command {
         Command::Init => match init() {
             Ok(_) => (),
-            Err(e) => eprintln!("Failed to initialize git repository: {:?}", e),
+            Err(e) => eprintln!("Failed to initialize git repository: {}", e),
         },
+        Command::CatFile { object: hash, pretty_print } => match cat_file(hash, pretty_print) {
+            Ok(_) => (),
+            Err(e) => eprintln!("cat-file failed: {}", e),
+        }
     }
 }
 
@@ -34,6 +47,30 @@ fn init() -> anyhow::Result<()> {
     fs::create_dir(".git/refs")?;
     fs::write(".git/HEAD", "ref: refs/heads/main\n")?;
     println!("Initialized git directory");
+
+    Ok(())
+}
+
+fn cat_file(hash: String, pretty_print: bool) -> anyhow::Result<()> {
+    let mut file_path = PathBuf::from(".git/objects");
+    file_path.push(&hash[..2]);
+    file_path.push(&hash[2..]);
+    let mut file = BufReader::new(ZlibDecoder::new(File::open(file_path)?));
+    // std::io::copy(&mut file, &mut &std::io::stdout())?;
+    let mut buffer = vec![];
+    file.read_until(b' ', &mut buffer)?;
+    let obj_type = std::str::from_utf8(&buffer)?.trim_end().to_owned();
+
+    buffer.clear();
+    file.read_until(0, &mut buffer)?;
+    let obj_size = std::str::from_utf8(&buffer)?.trim_end_matches('\0').parse::<u64>()?;
+
+    eprintln!("Object type: {}", obj_type);
+    eprintln!("Object size: {}", obj_size);
+
+    if pretty_print {
+        std::io::copy(&mut file, &mut &std::io::stdout())?;
+    }
 
     Ok(())
 }
